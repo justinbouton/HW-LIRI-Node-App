2aaa3d1920f0b302fbf7f4d83bcd0dbfb74ca178
justbouton <justb1985@gmail.com>
2018-11-25 20:11:36 -0800

    Major updates to song function. Converted songSeach function to a promise, created for loop to handle multple words, returned output(artist, title, album, url) from JSON results.

--- LIRI-node-app.js
+++ LIRI-node-app.js
@@ -1,14 +1,3 @@
-// 1. Search Spotify for a song:
-
-// `node liri.js spotify <song name>`
-
-// * Returns the following:
-//     * Artist(s)
-//     * Title
-//     * Album
-//     * A link to preview the song
-
-
 // 2. Search OMDB for a moive:
 
 // `node liri.js omdb <movie title>`
@@ -55,19 +44,44 @@
 
 //    * [DotEnv](https://www.npmjs.com/package/dotenv)
 
+
 // Step 1
 //Declare required var for APIs: Node-Spotify-api, OMDBapi, bandsintown
-var spotify = require("node-spotify-api")
-var omdb = require("omdbapi")
-var bandsInTown = require("bandsintownapi")
+var Spotify = require("node-spotify-api");
+
+// Input for switch
+var input1 = process.argv[2];
+
+// Store all of the arguments in an array
+var input2 = process.argv[3];
+
+// Create an empty variable for holding the song name
+var input2ConCat = "";
+
+var input2 = process.argv;
+
+var input2ConCat = "";
+
+// Loop through all the words in the node argument
+for (var i = 3; i < input2.length; i++) {
+    input2ConCat = input2ConCat + " " + input2[i];
+  }
+
+console.log("70 Search: " + input2ConCat.trim())
+
+// TODO // Create for loop to handle multiple words
+
+// var omdb = require("omdbapi")
+// var bandsInTown = require("bandsintownapi")
 
 
 // Step 2
 //Create switch statment to handle requests song, movie, band
-
-switch (action) {
+// console.log("71 input: " + input1)
+switch (input1) {
     case "song":
     songSearch();
+    // console.log('Switch "song"')
     break;
 
     case "movie":
@@ -80,9 +94,28 @@ switch (action) {
 }
 
 // If "songSearch" function is called
-function songSearch() {
-    //call Node-Spotify-api
-}
+    function songSearch() {
+    //  console.log("Mode: " + input1 +" Search: " + input2)   
+        var spotify = new Spotify({
+            id: "d242dbfec5244fdbbdcc2e1769a3119c",
+            secret: "5ee46c631ee341d6a114e9e34eb4d85b"
+        });
+
+        spotify
+            .search({ type: 'track', query: input2ConCat })
+            .then(function(data) {
+                console.log("ARTIST: " + JSON.stringify(data.tracks.items[0].artists[0].name))// Artist(s)
+                console.log("TITLE: " +JSON.stringify(data.tracks.items[0].name)) // Title
+                console.log("ALBUM: " +JSON.stringify(data.tracks.items[0].album.name)) // Album
+                console.log("CMD + DBL CLICK -> " + JSON.stringify(data.tracks.items[0].artists[0].external_urls.spotify)) // A link to preview the song
+            })
+            .catch(function(err) {
+                console.log('Error occurred: ' + err);
+            });
+        // TODO* If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+      };
+
 
 // If "movieSearch" function is called
 function movieSearch() {
